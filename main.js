ff9ff10906768420a71eb5864c3fb85cd80ed833
Emil Backlund <emil_backi_@hotmail.com>
2022-09-10 19:35:12 +0200

    snake game complete

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
 } from "/snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 //  Almost any game you are going to work on
 //  is going to be broken into two different steps
 
@@ -14,9 +18,13 @@ import {
 //  base on the "update loop"
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    return alert("You lose");
+  }
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -31,9 +39,16 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
